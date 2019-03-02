5eb0b3e702de6a2bb24b4b36baaba47d97e1fc21
Nick McClorey <mcclorns@mail.uc.edu>
2019-03-02 14:53:17 -0500

    added scene

--- Ninja Cat Jam/Assets/Scripts/HelloUnity.cs
+++ Ninja Cat Jam/Assets/Scripts/HelloUnity.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class HelloUnity : MonoBehaviour
 {
+    public SpriteRenderer myBoy;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class HelloUnity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Transform transform = GetComponent<Transform>();
+        if (transform.position.y < -3) {
+            var x = GetComponent<Rigidbody2D>();
+            x.velocity = new Vector3(0, 0);
+            transform.position = new Vector3(0, 0);
+        }
     }
 }
