b1e42322d83075cf18688273d89f9dfe1d966599
dkarvelis <79dkarvelis@gmail.com>
2019-03-02 23:55:09 -0500

    CameraFollowerAdded
    added script that makes camera follow the player

---
+++ Ninja Cat Jam/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 distance;
+    void Start()
+    {
+        distance = transform.position - Player.transform.position;
+    }
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + distance;
+    }
+}
